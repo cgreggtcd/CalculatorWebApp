f7a1c83b4d8b935e521aa89de9dfb9d1eeff0a00
cgreggtcd <98959891+cgreggtcd@users.noreply.github.com>
2022-10-21 11:29:42 +0100

    Separate tests into different files
    Also tests that tests can fail

--- src/test/java/com/softenggroup5/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/softenggroup5/calculator/CalculatorApplicationTests.java
@@ -3,20 +3,10 @@ package com.softenggroup5.calculator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @SpringBootTest
-//import static org.junit.Assert.assertEquals;
-//import org.junit.Test;
- 
 public class CalculatorApplicationTests
 {
     @Test
     void contextLoads() {
-    }
-
-    @Test
-    void testCalculate() {
-        assertEquals("0", Calculator.calculate(""));
     }
 }
---
+++ src/test/java/com/softenggroup5/calculator/CalculatorTests.java
@@ -0,0 +1,15 @@
+package com.softenggroup5.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class CalculatorTests
+{
+    @Test
+    void testCalculate() {
+        assertEquals("1", Calculator.calculate(""));
+    }
+}
